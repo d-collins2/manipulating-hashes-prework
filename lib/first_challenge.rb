3ced3855b0d442b34d3c7c95ab083389cfe64149
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-29 00:31:44 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,11 @@ def first_challenge
   }
 
   #your code here
+  contacts.each do |k, v|
+    k.each do |k1, v1| 
+      v1.delete(strawberry)
+    end 
+  end 
 
 
   #remember to return your newly altered contacts hash!
